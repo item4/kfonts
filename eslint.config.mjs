a9040e502d92ad7df22df2b6ea31480def4d660c
Kim Jinsu <item4hun@gmail.com>
2025-02-23 10:11:12 +0900

    Use `tseslint.config` for target files correctly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,25 @@
 import css from '@eslint/css';
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
-import prettier from 'eslint-plugin-prettier/recommended';
+import prettierPluginConfigsRecommended from 'eslint-plugin-prettier/recommended';
 import unicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
     ignores: ['node_modules', '.cache', '.yarn', 'packages/**/*'],
   },
-  js.configs.recommended,
-  // eslint-disable-next-line import/no-named-as-default-member
-  tseslint.configs.recommended,
-  unicorn.configs.recommended,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
-  prettier,
-  {
+  ...tseslint.config({
+    files: ['**/*.ts', '**/*.tsx', '**/*.mjs'],
+    extends: [
+      js.configs.recommended,
+      // eslint-disable-next-line import/no-named-as-default-member
+      tseslint.configs.recommended,
+      unicorn.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+      prettierPluginConfigsRecommended,
+    ],
     settings: {
       'import/resolver': {
         typescript: true,
@@ -25,9 +28,6 @@ export default tseslint.config(
       'import/extensions': ['.js', '.cjs', '.mjs', '.ts', '.tsx'],
       'import/internal-regex': '^@/',
     },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
     rules: {
       '@typescript-eslint/ban-ts-comment': [
         'error',
@@ -70,10 +70,10 @@ export default tseslint.config(
       ],
       'unicorn/prevent-abbreviations': 'off',
     },
-  },
+  }),
   {
     files: ['**/*.css'],
     language: 'css/css',
     ...css.configs.recommended,
   },
-);
+];
